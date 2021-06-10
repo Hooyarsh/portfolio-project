3c60dff5cb01645549b59b3390fa91b1c15c0a77
Hooyar <hooyarsh@gmail.com>
2021-06-10 23:17:20 +0430

    added blogs

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Blog
+
+def allblogs(request):
+	blogs = Blog.objects
+	return render(request, 'blog/allblogs.html', {'blogs':blogs})
